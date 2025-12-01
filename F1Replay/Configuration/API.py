601179ebf5bc4f9dbe6458f69c0cea32864592c0
Nikola <133783612+Niko2357@users.noreply.github.com>
2025-12-01 21:55:17 +0100

    Add files via upload

---
+++ F1Replay/Configuration/API.py
@@ -0,0 +1,44 @@
+import requests
+from Configuration.ConfigManager import ConfigManager
+
+
+class API:
+    @staticmethod
+    def fetch_driver():
+        """
+        Fetches information by API, gets driver name and team.
+        :return: object driver information or exception
+        """
+        config = ConfigManager.open_config("config")
+        api = config["race"]["api_drivers"]
+        session = config["race"]["session_key"]
+        try:
+            response = requests.get(api, params={"session_key": session})
+            response.raise_for_status()
+            drivers = response.json()
+            details = {}
+            for driver in drivers:
+                details[driver["driver_number"]] = {
+                    "name": f"{driver.get('first_name', '')} {driver.get('last_name', '')}",
+                    "team": driver.get("team_name", "N/A")
+                }
+            return details
+        except Exception as e:
+            return f"Details couldnt be loaded. {e}"
+
+    @staticmethod
+    def fetch_session_results():
+        """
+        Gets information about finished race from API.
+        :return: whole API information about race results
+        """
+        config = ConfigManager.open_config("config")
+        api = config["race"]["api_session_results"]
+        session = ConfigManager.open_config("key")
+        try:
+            response = requests.get(api, params={"session_key": session})
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            print(f"API couldnt be loaded: {e}")
+            return []
